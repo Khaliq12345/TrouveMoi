cbf68feadfc553fd87d65cf4a6c98e3d750aed22
wallcreation <lanhajames@gmail.com>
2026-04-08 19:06:35 +0100

    Modify Event header title to match same style as support header title

--- app/components/Event/EventHeader.vue
+++ app/components/Event/EventHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pa-2">
-    <h1 class="text-h5 text-sm-h3 font-weight-black">Événements</h1>
+    <h1 class="text-h5 font-weight-bold text-grey-darken-4 tracking-tight">Événements</h1>
     <span class="text-caption text-uppercase font-weight-medium text-grey">
       Tableau de bord de gestion
     </span>
